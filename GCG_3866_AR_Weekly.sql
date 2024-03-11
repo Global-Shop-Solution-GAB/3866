c469a47afda2974368f20664205c07b950763b25
Service Web <sw@gssmail.com>
2024-03-11 08:49:59 -0500

    Commit changes in C:\Websites\Git\emarlow\3866\GCG_3866_AR_Weekly.sql

---
+++ GCG_3866_AR_Weekly.sql
@@ -0,0 +1,14 @@
+DROP TABLE "GCG_3866_AR_Weekly";
+CREATE TABLE "GCG_3866_AR_Weekly"(
+ "Terminal" VARCHAR(3),
+ "Customer" VARCHAR(6),
+ "Invoice" VARCHAR(7),
+ "Invoice_Date" DATE,
+ "Due_Date" DATE,
+ "Terms_Code" VARCHAR(1),
+ "Curr_Week" DOUBLE,
+ "Week_1" DOUBLE,
+ "Week_2" DOUBLE,
+ "Week_3" DOUBLE,
+ "Week_4" DOUBLE,
+ "Week_5" DOUBLE);
